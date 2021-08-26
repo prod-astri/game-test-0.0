a5654aece110f9da578ff72d23fe88e4e64766b0
prod.astri <58519454+prod-astri@users.noreply.github.com>
2021-08-26 17:36:22 +0200

    make cannon rotate

--- js/cannon.js
+++ js/cannon.js
@@ -6,21 +6,34 @@ class Cannon {
         // the x position is relative to the hill position!
         this.x = game.state.blocksLeft/2
         this.y = game.background.playerFloorHeight - this.coreSize/2;
-
+        
         this.angle = game.angleMomentary;
         
         console.log(`%c cannon starting angle = ${this.angle}`, `color: lightgreen`)
         console.log(`%c / cannon constructor`, `color: green`)
     }
-
+    
     draw(){
         strokeWeight(1);
         fill('azure')
         circle(this.x, this.y, this.coreSize);
-
-
-        //// missing rotation
-        rect(this.x-this.coreSize/2, this.y-this.coreSize, this.coreSize, this.coreSize)
+        
+        // rotate the cannon mouth
+        push()
+        
+        angleMode(DEGREES)
+        translate(this.x, this.y);
+        rotate(-game.angleMomentary);
+        translate(0, -this.coreSize/2);
+        rectMode(CENTER)
+        rect(0, 0,this.coreSize, this.coreSize)
+        
+        pop();
+        
+        
+        
+        
+        
         // console.log(`%c cannon angle = ${game.angleMomentary}`, `color: salmon`)
     }
 }
